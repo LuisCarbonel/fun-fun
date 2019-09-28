d1d403a9aee4631491a23698e0104a501f99ab0f
bingi <luis.carbonel.gk@hotmail.com>
2019-09-27 20:37:09 -0400

    buttons

---
+++ buttons/src/app/components/buttons/button.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {ButtonComponent} from "./button.component";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    ButtonComponent,
+  ]
+
+})
+export class ButtonModule {}
